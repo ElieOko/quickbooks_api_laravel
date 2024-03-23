0bb854463e1d5e8ca08e2e964ee19df616e1f188
elieoko <elieoko100@gmail.com>
2024-03-23 11:36:28 +0100

    fix : new endpoint

--- routes/api.php
+++ routes/api.php
@@ -12,11 +12,12 @@ use App\Http\Controllers\UserController;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
-|processCode/refresh_token/getInvoiceQueryAll
+|processCode/refresh_token/getInvoiceQueryAll//
 */
 
 Route::get('/user/oauth', [UserController::class, 'oauth_quickbooks']);
 Route::get('/callback', [UserController::class, 'processCode']);
+Route::get('/redirect', [UserController::class, 'redirect_to']);
 Route::get('/refresh_token/quickbook/{token}', [UserController::class, 'refresh_token']);
 Route::get('/qb/invoice/all', [UserController::class, 'getInvoiceQueryAll']);
 
