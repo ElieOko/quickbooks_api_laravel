a5861b89abf8dc42d9b93e5de6dcd19717bb106f
elieoko <elieoko100@gmail.com>
2024-03-25 13:43:26 +0100

    fix : call render

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use QuickBooksOnline\API\DataService\DataService;
@@ -78,13 +79,16 @@ class UserController extends Controller
         $token = $accessTokenJson["access_token"];
         // $client = new Client(['headers' => [
         //     'Authorization' => "Bearer $token"
-        // ]]);
+        // ]]); $accessTokenJson
         // header("https://qkbfront.drapeauyamboka.com/callback");
         //$client->get('https://qkbfront.drapeauyamboka.com/login');
         //header("https://qkbfront.drapeauyamboka.com/callback");
-        return response()->json([
-            "token" => $accessTokenJson,
+        return Inertia::render('Welcome', [
+            'token' => $token,
         ]);
+        // return response()->json([
+        //     "token" => $accessTokenJson,
+        // ]);
         /*
      * Setting the accessToken for session variable
      */
