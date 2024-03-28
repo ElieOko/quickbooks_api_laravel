754c49f087427b46d16db455a46c62613b0e51d5
ElieOko <elieoko100@gmail.com>
2024-03-28 07:15:32 +0100

    build : project test

--- resources/js/utils/service/axios_api.ts
+++ resources/js/utils/service/axios_api.ts
@@ -36,8 +36,6 @@ export const useAx = (baseurl:string="quickbook.drapeauyamboka.com") : AxiosInst
         "X-Requested-With"  :   "XMLHttpRequest",
         "Authorization"     :   "Bearer eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..UFV3k2WVBU9N7LlpZHHK1w.LPnbWUV6tzt5CUJVGKPME9PxCo84K7nwztPfv1RMVmi9izjKo7y2jksvJBXxGZ_8185mgebN_ox5JIsv5PWSzJLygsYtsfEy3jYkTdRFLftx5EnzGeODAVPAvSgVPGt4zQuD744RyUC0lWuV4rRRK8Ckwzil7NIXrBUHYLyjjCiMRFUAnqMP99t26IQ12zkdpuQvbpXiFx4NfTaqo1c6fasUfpOPJMZvvFerVX-X0uVGyqYkie69FsiOQQJQaEVz0oRMMSYtvFbvkHFyM1otC06yAGzT9DbvewTB6D0ZUtgBEZIZi7AXSKJ49I-IIL1usPbEvBDTXH1OV1eLpHEFsp0AstHAaQB0Ow-LFIGWTP4-0khJRqTAwXbkM43e8zpnj3SenO2poAIlRnmf6vhQHf6riYkxgKmPKw0jxiB5skqvx3aLaWK3GjGnW5ZGc7W2ikPlAFbMPK-r1V1iMBEOdcDAxPmDj0HPnmTRjghHV5NmDYjaag2p2SlWkoTDJtDJ9cHaUNkasdgX2R7A5ZJftEjR_Rde3rgMjiEqqERQc8K60wESFk_iqfPX3JeAHLW_XPmpKfPsllW3e9Z2hlnQDdVr3Z3RuYRDk1endhPo__Pn7vfWAein75xJ9RcrtMbgRINBXOlo8JQ0E0-m9DxjsXs84JTjelWxvaV3NEzPtwicepMgCtHCVrBHhO1NNe03VyF4kZu9fjLgy_Iet2ly5sFbwCCXibusU7aFnKtZ_C32hh8D6feCd7pnQ8zdQnNW.2dtHH-BIzR2OUxwMFsQiLw",
       },
-      data : "select * from employee startposition 1 maxresults 5",
-      
     })
     return useAxios
   }
