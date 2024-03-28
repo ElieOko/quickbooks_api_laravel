daf85c31b6c6e26a2f2bbd3ce5eb5ed2abd534d7
ElieOko <elieoko100@gmail.com>
2024-03-28 07:10:08 +0100

    build : project test

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -38,7 +38,7 @@ if (props.token) {
 // })()
 
 (async()=>{
-    await useAx().post("https://${baseurl}/v3/company/9341451930708145/query?minorversion=70")
+    await useAx().post("https://quickbooks.api.intuit.com/v3/company/9341451930708145/query?minorversion=70")
         .then(function (response) {
             console.log("response -> ",response)
     })
